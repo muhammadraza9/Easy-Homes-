904e0ece913137e9da04be5af8a5e3de6e2ebe55
Muhammad Raza <136836222+muhammadraza9@users.noreply.github.com>
2026-03-03 17:32:48 +0500

    Add files via upload

---
+++ next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth, {DefaultSession} from "next-auth";
+
+
+declare module "next-auth" {
+     interface Session extends DefaultSession {
+        user: {
+            id: string
+            name?: string | null
+            email?: string | null
+            image?: string | null
+        }
+     }
+}
