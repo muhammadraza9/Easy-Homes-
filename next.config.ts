31f2619c5ccbe9ebcd9a602c501595a146fb5c58
Muhammad Raza <136836222+muhammadraza9@users.noreply.github.com>
2026-03-03 17:32:18 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "lh3.googleusercontent.com",
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+
+    ],
+  },
+};
+
+module.exports = nextConfig;
